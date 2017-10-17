c7e3d9e5e6e2d8517a8c1d7832dc25f26edc2e4a
SkyFishT <569735779@qq.com>
2017-10-17 08:34:48 +0800

    new

--- static/js/admin_io.js
+++ static/js/admin_io.js
@@ -6,33 +6,29 @@ pop_layer = document.getElementById("pop_layer");//弹出层
 pop_layer.style.zIndex = document.getElementById("btn_stu_add").style.zIndex + 1;//弹出层覆盖
 //元素获取
 form_stu_edit = document.getElementById("form_stu_edit");
-stu_labels = form_stu_edit.getElementsByTagName("label");
-stu_inputs = form_stu_edit.getElementsByTagName("input");
-temp_stu_labels = [];
-temp_stu_inputs = [];
+form_nodes = form_stu_edit.childNodes;
+form_temp_nodes = [];
 //固定标签和输入框的引用
-for (var i = 0; i < stu_labels.length; i++) {
-    temp_stu_inputs[i] = stu_inputs[i + 1];
-    temp_stu_labels[i] = stu_labels[i];
+for (var i = 2,j = 0; i < form_nodes.length;) {
+    console.log("temp["+j+"]:"+form_temp_nodes[j]+",nodes["+i+"]:"+form_nodes[i]);
+    form_temp_nodes[j++] = form_nodes[i++];
+    if(i%3==1) i++;
 }
-//固定submit按钮
-temp_stu_inputs[stu_labels.length] = stu_inputs[stu_labels.length+1];
+
 //添加每组输入框到span里
-for (var i = 0; i < stu_labels.length; i++) {
+for (var i = 0; i < form_temp_nodes.length; i+=2) {
     //console.log(stu_labels[i].textContent)
-    (function (label, input) {
+    (function (label, elem) {
         var span = document.createElement("span");
         span.appendChild(label);
-        span.appendChild(input);
+        if(elem)
+            span.appendChild(elem);
         form_stu_edit.appendChild(span);
-    })(temp_stu_labels[i], temp_stu_inputs[i])
+    })(form_temp_nodes[i], form_temp_nodes[i+1])
 }
-document.getElementById("form_stu_edit").getElementsByTagName("span")[stu_labels.length-1].style.display="none";
+spans = form_stu_edit.getElementsByTagName("span");
+spans[spans.length-2].style.display="none";
 var which_button = document.getElementById("id_which_button");
-//添加submit按钮到span
-var span = document.createElement("span");
-span.appendChild(temp_stu_inputs[stu_labels.length]);
-form_stu_edit.appendChild(span);
 
 function btn_stu_add() {
     pop_layer.style.display = "block";
@@ -41,13 +37,13 @@ function btn_stu_add() {
 
 function btn_stu_change(event) {
     pop_layer.style.display = "block";
-    //temp_stu_inputs[0].disabled = "disabled";
+    form_temp_nodes[1].readonly="readonly";
     var event = event ? event : window.event;
     var obj = event.srcElement ? event.srcElement : event.target;
     var parent = obj.parentNode.parentNode;
     var divs = parent.getElementsByTagName("div");
     for (var i = 0; i < divs.length; i++) {
-        temp_stu_inputs[i].value = divs[i].innerHTML;
+        form_temp_nodes[i*2+1].value = divs[i].innerHTML;
     }
     which_button.setAttribute("value", "modify");
 }
@@ -59,7 +55,7 @@ function btn_stu_del(event) {
         var parent = obj.parentNode.parentNode;
         var divs = parent.getElementsByTagName("div");
         for (var i = 0; i < divs.length; i++) {
-            temp_stu_inputs[i].value = divs[i].innerHTML;
+            form_temp_nodes[i*2+1].value = divs[i].innerHTML;
         }
         which_button.setAttribute("value", "delete");
         form_stu_edit.submit();
