8d2ca013b592091e1edecb87fcd7b0125750daeb
SkyFishT <569735779@qq.com>
2017-11-11 17:34:21 +0800

    1

--- students/views.py
+++ students/views.py
@@ -5,9 +5,9 @@ from students.models import Students
 # Create your views here.
 
 def in_reading(request):
-    in_reading = Students.objects(in_reading="TRUE")
+    in_reading = Students.objects(in_reading="TRUE").order_by("enrollment")
     return render(request,'students/in_reading.html',{'in_reading':in_reading})
 
 def graduates(request):
-    graduates = Students.objects(in_reading="FALSE")
+    graduates = Students.objects(in_reading="FALSE").order_by("enrollment")
     return render(request,'students/graduates.html',{'graduates':graduates})
