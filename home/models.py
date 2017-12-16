ff381f27c1635c1256574bb1f85c807bc7e0ad9b
SkyFishT <569735779@qq.com>
2017-12-16 20:23:08 +0800

    1

--- home/models.py
+++ home/models.py
@@ -1,6 +1,12 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
+from mongoengine import *
 
 from django.db import models
 
 # Create your models here.
+class Messages(Document):
+    msg = StringField(max_length=500)
+
+class Achievements(Document):
+    achieve = StringField(max_length=500)
--- home/views.py
+++ home/views.py
@@ -2,9 +2,10 @@
 from __future__ import unicode_literals
 
 from django.shortcuts import render
+from home.models import Messages,Achievements
 # Create your views here.
 
 def index(request):
-    home_msg = "欢迎访问Tanglab网站"
-    home_msgs = ["新闻1","新闻2"]
-    return render(request,'home/index.html',{'home_msg':home_msg,'home_msgs':home_msgs})
+    home_msgs = Messages.objects()
+    home_achieve = Achievements.objects()
+    return render(request,'home/index.html',{'home_msgs':home_msgs,'home_achieve':home_achieve})
